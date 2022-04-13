04b4b69988b6d0024f56ab286d665943afe1270d
sujay31 <sujay31rastogi@gmail.com>
2022-04-13 21:30:25 +0530

    drop queries added
    comments also added

--- SQL_QUERIES.sql
+++ SQL_QUERIES.sql
@@ -1,5 +1,4 @@
 -- Created required database and tables  
-
 CREATE DATABASE hostel;
 USE hostel;
 SHOW DATABASES;
@@ -12,7 +11,7 @@ PRIMARY KEY (hostel_code)
 );
 
 CREATE TABLE wing_leader(
-id VARCHAR (13) NOT NULL UNIQUE,
+id CHAR (13) NOT NULL UNIQUE,
 wing_size INT UNSIGNED NOT NULL,
 gender CHAR(1), CHECK(gender in ("M", "F")),
 pref_1 CHAR(2) NOT NULL,
@@ -22,8 +21,8 @@ PRIMARY KEY (id)
 );
 
 CREATE TABLE wing_members(
-id_1 VARCHAR (13) NOT NULL,
-id_2 VARCHAR (13),
+id_1 CHAR (13) NOT NULL,
+id_2 CHAR (13),
 gender CHAR(1), CHECK(gender in ("M", "F")),
 PRIMARY KEY (id_1),
 FOREIGN KEY (id_2) REFERENCES wing_leader(id)
@@ -49,10 +48,12 @@ CREATE TABLE sr LIKE ashok;
 CREATE TABLE vishwakarma LIKE ashok;
 CREATE TABLE vyas LIKE ashok;
 
+-- inserting sample data 
 INSERT INTO hostel_list VALUES
 ("AK", 400), ("BG", 400), ("BD", 400), ("CV", 400), ("GN", 400), ("KR", 400), ("ML", 400),
 ("MR", 400), ("RM", 400), ("RP", 400), ("SK", 400), ("SR", 400), ("VK", 400), ("VY", 400);
 
+-- trigger to maintain consistency in inserted tuples 
 DELIMITER $$
 CREATE TRIGGER trig BEFORE INSERT
 ON wing_members
@@ -68,12 +69,15 @@ FROM
 	wing_members
 WHERE 
 	wing_members.id_2 = NEW.id_2;
-IF (x > (SELECT wing_size - 2 FROM wing_leader WHERE wing_leader.id = NEW.id_2)) THEN
+IF (x > (SELECT wing_size - 1 FROM wing_leader WHERE wing_leader.id = NEW.id_2)) THEN
 	SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = 'Insert Unsuccessful. Wing is already full.';
 END IF;
 END; $$
 DELIMITER ;
 
+-- DROP TRIGGER trig;
+
+-- Sample data for wing_leader table 
 INSERT INTO wing_leader VALUES('2019A1PS0150P', 3, 'M', 'ML', 'RM', 'BD');
 INSERT INTO wing_leader VALUES('2019A1PS0599P', 4, 'M', 'ML', 'RM', 'BD');
 INSERT INTO wing_leader VALUES('2019A1PS0615P', 5, 'M', 'SR', 'RM', 'KR');
@@ -82,8 +86,9 @@ INSERT INTO wing_leader VALUES('2019B4A70860P', 3, 'F', 'MR', NULL, NULL);
 INSERT INTO wing_leader VALUES('2019A2PS0414P', 7, 'M', 'AK', 'RM', 'SK');
 INSERT INTO wing_leader VALUES('2019A1PS0433P', 3, 'F', 'MR', NULL, NULL);
 
-SELECT * FROM wing_leader;
+SELECT * FROM wing_members;
 
+-- Sample data for wing_members table
 INSERT INTO wing_members VALUES('2019A1PS0386P', '2019A1PS0150P', 'M');
 INSERT INTO wing_members VALUES('2019A1PS0556P', '2019A1PS0150P', 'M');
 INSERT INTO wing_members VALUES('2019A1PS0581P', '2019A1PS0150P', 'M');
@@ -125,12 +130,15 @@ INSERT INTO wing_members VALUES('2019A1PS0711P', '2019A1PS0433P', 'F');
 
 select * from wing_members;
 
-update wing_leader set wing_size = wing_size + 1;
+-- As the input value excluded the wing_leader in the count of wing_members 
+UPDATE wing_leader SET wing_size = wing_size + 1;
 
 -- Creating required views and procedures
 CREATE VIEW girls AS
 SELECT id, wing_size FROM wing_leader
-where gender = "F";
+WHERE gender = "F";
+
+SELECT * FROM girls;
 
 delimiter $$
 CREATE PROCEDURE get_vacancy (IN hostel_abr CHAR(2), OUT vacant INT)
@@ -151,24 +159,24 @@ BEGIN
 	SET i=0;
     
     WHILE i<n DO 
-		Select id, wing_size
-        into @id, @size
-        from girls
-        order by wing_size desc
+		SELECT id, wing_size
+        INTO @id, @size
+        FROM girls
+        ORDER BY wing_size DESC
         LIMIT i,1;
-        call get_vacancy("MR", @vacant);
-        if @vacant >= @size then
-			call allocate_girls(@id);
-		else
+        CALL get_vacancy("MR", @vacant);
+        IF @vacant >= @size THEN
+			CALL allocate_girls(@id);
+		ELSE
 			SELECT 'Not enough rooms' AS '';
-        end if;
+        END IF;
 		SET i = i + 1;
 	END WHILE;
 END $$ 
 DELIMITER ; 
 
 delimiter $$
-CREATE PROCEDURE allocate_girls (in leader_id CHAR(13))-- , in hostel_abr CHAR(2))
+CREATE PROCEDURE allocate_girls (IN leader_id CHAR(13))
 COMMENT 'Allots a room in Meera Bhawan to every girl in a wing. Room & ID gets stored in the meera table'
 BEGIN
     DECLARE i INT DEFAULT 0;     
@@ -178,26 +186,26 @@ BEGIN
     
     WHILE i<@size-1 DO
 		
-        SELECT id_1 into @mem from wing_members where id_2=leader_id limit i,1;
-		set i = i + 1;
-        INSERT INTO meera value
+        SELECT id_1 INTO @mem FROM wing_members WHERE id_2=leader_id LIMIT i,1;
+		SET i = i + 1;
+        INSERT INTO meera VALUE
 		(400 - @vacant + i, @mem);
         
-	end while;
-    set i = i+1;
-	INSERT INTO meera value
+	END WHILE;
+    SET i = i + 1;
+	INSERT INTO meera VALUE
 	(400 - @vacant + i, leader_id);
     
-    update hostel_list 
-	set vacancy = @vacant - @size
-	where hostel_code = "MR";
+    UPDATE hostel_list 
+	SET vacancy = @vacant - @size
+	WHERE hostel_code = "MR";
     
 END $$
 
 call hostel_allotment_girls();
 
-select * from hostel_list;
-select * from meera;
+SELECT * FROM hostel_list;
+SELECT * FROM meera;
 
 
 -- Starting boys allocation 
@@ -207,7 +215,8 @@ where gender = "M";
 
 DELIMITER $$
 CREATE PROCEDURE hostel_allotment_boys()  
-COMMENT 'Gets id of every boy wing leader and calls the allocate_boys() procedure to allot room to each boy in their preferrerd Bhawan'
+COMMENT 'Gets id of every boy wing leader and calls the allocate_boys() procedure 
+		to allot room to each boy in their preferrerd Bhawan'
 BEGIN  
 
     DECLARE n INT DEFAULT 0;
@@ -217,25 +226,25 @@ BEGIN
 	SET i=0;
     
     WHILE i<n DO 
-		Select id, wing_size, pref_1, pref_2, pref_3
-        into @id, @size, @pref_1, @pref_2, @pref_3
-        from boys
-        order by wing_size desc
+		SELECT id, wing_size, pref_1, pref_2, pref_3
+        INTO @id, @size, @pref_1, @pref_2, @pref_3
+        FROM boys
+        ORDER BY wing_size DESC
         LIMIT i,1;
 
-        call get_vacancy(@pref_1, @vacant_1);
-        call get_vacancy(@pref_2, @vacant_2);
-        call get_vacancy(@pref_3, @vacant_3);
+        CALL get_vacancy(@pref_1, @vacant_1);
+        CALL get_vacancy(@pref_2, @vacant_2);
+        CALL get_vacancy(@pref_3, @vacant_3);
 
-        if @vacant_1 >= @size then
-			call allocate_boys(@id, @pref_1); -- need to correct here
-		elseif @vacant_2 >= @size then 
-			call allocate_boys(@id, @pref_2);
-		elseif @vacant_3 >= @size then 
-			call allocate_boys(@id, @pref_3);
-		else
-			call random_allocate_boys(@id, @size);
-        end if;
+        IF @vacant_1 >= @size THEN
+			CALL allocate_boys(@id, @pref_1); 
+		ELSEIF @vacant_2 >= @size THEN 
+			CALL allocate_boys(@id, @pref_2);
+		ELSEIF @vacant_3 >= @size THEN 
+			CALL allocate_boys(@id, @pref_3);
+		ELSE
+			CALL random_allocate_boys(@id, @size);
+        END IF;
 
 		SET i = i + 1;
 	END WHILE;
@@ -244,32 +253,34 @@ DELIMITER ;
 
 delimiter $$
 CREATE PROCEDURE allocate_boys (IN leader_id CHAR(13), IN hostel_abr CHAR(2))
+COMMENT 'Allocates room to every boy in the wing in the hostel of their preference, 
+		if possible, otherwise they are allotted rooms randomly'
 BEGIN
 	
     DECLARE i INT DEFAULT 0;
-    call get_vacancy(hostel_abr, @vacant);
+    CALL get_vacancy(hostel_abr, @vacant);
 
     SELECT wing_size INTO @size FROM wing_leader WHERE id=leader_id;
 
     WHILE i<@size-1 DO  
-        SELECT id_1 into @mem from wing_members where id_2=leader_id limit i,1;
-		set i = i + 1;
-        call insert_into_hostel(hostel_abr, i, @mem);
+        SELECT id_1 INTO @mem FROM wing_members WHERE id_2=leader_id LIMIT i,1;
+		SET i = i + 1;
+        CALL insert_into_hostel(hostel_abr, i, @mem);
     END WHILE; 
 
     SET i = i+1;
-    call insert_into_hostel(hostel_abr, i, leader_id);
+    CALL insert_into_hostel(hostel_abr, i, leader_id);
 	
     UPDATE hostel_list
     SET vacancy = @vacant- @size
     WHERE hostel_code = hostel_abr;
-end $$
+END $$
 
 delimiter $$
 CREATE PROCEDURE insert_into_hostel (IN hostel_abr CHAR(2), IN i INT, IN id CHAR(13))
-COMMENT 'Returns the number of vacant room in a Bhawan'
+COMMENT 'Allocates the first non-occupied room in the accepted Bhawan to the accepted id'
 BEGIN
-	call get_vacancy(hostel_abr, @vacant);
+	CALL get_vacancy(hostel_abr, @vacant);
     CASE
 		WHEN hostel_abr = "AK" THEN INSERT INTO ashok VALUE(400-@vacant+i, id);
 		WHEN hostel_abr = "BG" THEN INSERT INTO bhagirath VALUE(400-@vacant+i, id);
@@ -284,7 +295,7 @@ BEGIN
 		WHEN hostel_abr = "SR" THEN INSERT INTO sr VALUE(400-@vacant+i, id);
 		WHEN hostel_abr = "VK" THEN INSERT INTO vishwakarma VALUE(400-@vacant+i, id);
 		WHEN hostel_abr = "VY" THEN INSERT INTO vyas VALUE(400-@vacant+i, id);
-    END case;
+    END CASE;
 END $$
 
 delimiter $$
@@ -294,16 +305,16 @@ BEGIN
 	DECLARE i INT DEFAULT 0;
     SELECT hostel_code, vacancy INTO @hostel_abr, @vacant 
     FROM hostel_list 
-    where hostel_code not in ("MR") ORDER BY vacancy desc, hostel_code asc limit 1;
+    WHERE hostel_code NOT IN ("MR") ORDER BY vacancy DESC, hostel_code ASC LIMIT 1;
     IF @vacant >= size THEN 
 		WHILE i<@size-1 DO  
-			SELECT id_1 into @mem from wing_members where id_2=leader_id limit i,1;
-			set i = i + 1;
-			call insert_into_hostel(@hostel_abr, i, @mem);
+			SELECT id_1 INTO @mem FROM wing_members WHERE id_2=leader_id LIMIT i,1;
+			SET i = i + 1;
+			CALL insert_into_hostel(@hostel_abr, i, @mem);
 		END WHILE; 
 	
 		SET i = i+1;
-		call insert_into_hostel(@hostel_abr, i, leader_id);
+		CALL insert_into_hostel(@hostel_abr, i, leader_id);
 		
 		UPDATE hostel_list
 		SET vacancy = @vacant- @size
@@ -315,11 +326,14 @@ END $$
 
 CALL hostel_allotment_boys();
 
-SELECT * FROM hostel_list;
+SELECT * FROM hostel_list; -- Observe the decrease in vacancies as we fill up students
+
+-- Example of allocation  
 SELECT * FROM ashok;
 SELECT * FROM ram;
 SELECT * FROM krishna;
 
+-- To test random allotment procedure, we have decreased the vacancies and added the additional tuples 
 update hostel_list set vacancy = 5 where hostel_code in ("Ak", "ML", "KR");
 
 INSERT INTO wing_leader VALUES('2019A5PS0414P', 8, 'M', 'AK', 'ml', 'KR');
@@ -332,4 +346,20 @@ INSERT INTO wing_members VALUES('2019A9PS0632P', '2019A5PS0414P', 'M');
 INSERT INTO wing_members VALUES('2019A9PS0640P', '2019A5PS0414P', 'M');
 INSERT INTO wing_members VALUES('2019A9PS0648P', '2019A5PS0414P', 'M');
 
-select * from meera;
+-- Drop queries for procedure, trigger, view, tables and database 
+DROP PROCEDURE get_vacancy;
+DROP PROCEDURE allocate_girls;
+DROP PROCEDURE hostel_allotment_girls;
+DROP PROCEDURE allocate_boys;
+DROP PROCEDURE random_allocate_boys;
+DROP PROCEDURE hostel_allotment_boys;
+DROP PROCEDURE insert_into_hostel;
+
+DROP TRIGGER trig;
+
+DROP VIEW girls, boys;
+
+DROP TABLES ashok, bhagirath, budh, cvr, gandhi, krishna, malviya, meera, ram, ranapratap, shankar, sr, vishwakarma, vyas;
+DROP TABLES wing_members, wing_leader, hostel_list;
+
+DROP DATABASE hostel;
